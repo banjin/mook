70179e1cdc563fcb914ad61e2c4a0beb13495073
songhm <songhm@qssec.com>
2017-12-03 15:46:37 +0800

    增加点击排序功能

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -15,10 +15,22 @@ class OrgView(View):
     def get(self, request):
         # 课程机构
         all_orgs = CourseOrg.objects.all()
-        org_nums = all_orgs.count()
+
+        hot_orgs=all_orgs.order_by('-click_num')
+
         # 城市
         all_citys = CityDict.objects.all()
 
+        # 对地区进行筛选
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_orgs = all_orgs.filter(city_id=int(city_id))
+
+        # 类别筛选
+        category = request.GET.get('ct', '')
+        if city_id:
+            all_orgs = all_orgs.filter(category=category)
+        org_nums = all_orgs.count()
         # 对课程机构进行分页
         try:
             page = request.GET.get('page', 1)
@@ -30,5 +42,10 @@ class OrgView(View):
         p = Paginator(all_orgs, 5, request=request)
 
         orgs = p.page(page)
-        return render(request, 'org-list.html', {'all_orgs': orgs, 'all_citys': all_citys, 'org_nums': org_nums})
+        return render(request, 'org-list.html', {'all_orgs': orgs,
+                                                 'all_citys': all_citys,
+                                                 'org_nums': org_nums,
+                                                 'city_id': city_id,
+                                                 'category': category,
+                                                 'hot_orgs': hot_orgs})
 
