9dd3bc2ebe24904251c1a00634a65efe405e94a8
songhm <songhm@qssec.com>
2017-12-05 13:05:46 +0800

    增加分页

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from .models import Course
 
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 
 class CourseListView(View):
     """
@@ -11,6 +13,18 @@ class CourseListView(View):
     """
     def get(self, request):
         all_course = Course.objects.all()
-        return render(request, 'course-list.html', {"all_course":all_course})
+
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        # Provide Paginator with the request object for complete querystring generation
+
+        p = Paginator(all_course, 3, request=request)
+        # 不是一个queryset对象，所以在模板中需要修改 {%for course in all_course.object_list%}
+        courses = p.page(page)
+
+        return render(request, 'course-list.html', {"all_course": courses})
 
 
